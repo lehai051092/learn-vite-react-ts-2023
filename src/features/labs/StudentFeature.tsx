4ef42a6e7d7fb9f598a2ce4aea01c481c699c2fe
Le Hai <le.thanh.hai@sun-asterisk.com>
2023-07-20 14:35:55 +0700

    Practice props

---
+++ src/features/labs/StudentFeature.tsx
@@ -0,0 +1,15 @@
+import { Student } from '../../models';
+
+export interface StudentFeatureProps {
+  student: Student;
+}
+
+export function StudentFeature({ student }: StudentFeatureProps) {
+  const { name, isHero } = student;
+
+  return (
+    <div>
+      Student: {name} {isHero ? 'is-hero' : 'no-hero'}
+    </div>
+  );
+}
